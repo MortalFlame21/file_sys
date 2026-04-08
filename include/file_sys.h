7f52e3c71fb98eb7097a95a4dfef367d469d07e6
MortalFlame21 <markgio2100@gmail.com>
2026-04-08 14:59:45 +1000

    fix: file_sys.h. sub_dir has levels.

--- include/file_sys.h
+++ include/file_sys.h
@@ -6,6 +6,6 @@ namespace bb {
     void copy(fs::path src, fs::path dst);
     void move(fs::path src, fs::path dst);
     fs::path rename_path(fs::path dst);
-    void print(fs::path p, int level = 0);
-    void print_subdir(fs::path p);
+    void print(fs::path p);
+    void print_subdir(fs::path p, int level = 0);
 };
