05daea0125362107bba783bc39303df796592145
MortalFlame21 <markgio2100@gmail.com>
2026-04-09 00:51:27 +1000

    feat: file_sys.h. update copy wrapper.

--- include/file_sys.h
+++ include/file_sys.h
@@ -3,9 +3,8 @@
 namespace fs = std::filesystem;
 
 namespace bb {
-    void copy(fs::path src, fs::path dst);
+    void copy(fs::path src, fs::path dst, fs::copy_options o);
     void move(fs::path src, fs::path dst);
-    fs::path rename_path(fs::path dst);
     void print(fs::path p);
     void print_subdir(fs::path p, int level = 0);
 };
